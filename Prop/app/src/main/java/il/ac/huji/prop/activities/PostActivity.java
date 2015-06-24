f48d25322004e4eb341a8474b9c0ee145200e051
elkbrsathuji <elkbrs@gmail.com>
2015-06-24 11:57:47 +0300

    post listeners

--- Prop/app/src/main/java/il/ac/huji/prop/activities/PostActivity.java
+++ Prop/app/src/main/java/il/ac/huji/prop/activities/PostActivity.java
@@ -6,7 +6,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 import il.ac.huji.prop.R;
 
 public class PostActivity extends ActionBarActivity {
@@ -23,15 +26,53 @@ public class PostActivity extends ActionBarActivity {
         setContentView(R.layout.activity_post);
         post = null;
         txt = (EditText) findViewById(R.id.post_text);
+
         bPhoto = (Button) findViewById(R.id.post_photo);
+        bPhoto.setOnClickListener(this);
         bVid = (Button) findViewById(R.id.post_vid);
+        bVid.setOnClickListener(this);
         bLoc = (Button) findViewById(R.id.post_location);
+        bLoc.setOnClickListener(this);
         bFile = (Button) findViewById(R.id.post_file);
+        bFile.setOnClickListener(this);
         bChooseProp = (Button) findViewById(R.id.post_choose_prop);
+        bChooseProp.setOnClickListener(this);
         bProp = (Button) findViewById(R.id.post_prop);
+        bProp.setOnClickListener(this);4
     }
 
+    public void onClick(View v){
+        switch(v.getId()){
 
+            case (R.id.post_photo):{
+                uploadPhoto();
+                break;
+            }
+            case (R.id.post_vid):{
+                uploaVid();
+                break;
+            }
+            case (R.id.post_location):{
+                uploadLoc();
+                break;
+                
+            }
+            case (R.id.post_file):{
+                uploadFile();
+                break;
+            }
+            case (R.id.post_choose_prop):{
+                chooseProp();
+                break;
+            }
+            case (R.id.post_prop):{
+                postProp();
+                break;
+            }
+
+
+
+        }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -54,3 +95,6 @@ public class PostActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+    private void postProp() {
+    }
