6b1aaeeec6bce187ddfcd604b114f54bab9429b5
elkbrsathuji <elkbrs@gmail.com>
2015-09-01 15:51:31 +0300

    layout changes and material design colors

---
+++ Prop/app/src/main/java/il/ac/huji/prop/models/services/DropboxService.java
@@ -0,0 +1,44 @@
+package il.ac.huji.prop.models.services;
+
+import il.ac.huji.prop.models.Post;
+
+/**
+ * Created by elkbrs on 31/08/15.
+ */
+public class DropboxService extends socialService {
+//    https://www.dropbox.com/developers/core/start/android
+
+    private  static String DROPBOX_CONSUMER_KEY = "tuvm4wau0iug0a0";
+    private static String DROPBOX_CONSUMER_SECRET = "njzx0os5zr7e37l";
+    private Post mPost;
+    // In the class declaration section:
+    private DropboxAPI<AndroidAuthSession> mDBApi;
+
+
+
+    public DropboxService() {
+        mPost = null;
+        initApi();
+    }
+    public DropboxService(Post p){
+        mPost = p;
+        initApi();
+    }
+
+    public Post getPost() {
+        return mPost;
+    }
+
+    public void setPost(Post p) {
+        this.mPost = p;
+    }
+    private void initApi(){
+        // And later in some initialization function:
+        AppKeyPair appKeys = new AppKeyPair(APP_KEY, APP_SECRET);
+        AndroidAuthSession session = new AndroidAuthSession(appKeys);
+        mDBApi = new DropboxAPI<AndroidAuthSession>(session);
+
+    }
+
+
+}
